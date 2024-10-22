601a274dca54f059bc034fb683a3772eaf754a7f
Lennart Kroll <24440161+lkroll@users.noreply.github.com>
2024-10-23 00:23:13 +0200

    Check for qmlls and qmlls6 binaries

--- src/qmljs.rs
+++ src/qmljs.rs
@@ -15,15 +15,18 @@ impl QmlJsExtension {
             return Ok(path.clone());
         }
 
-        let binary_name = "qmlls";
+        // Check for qmlls and qmlls6 binary names
+        let binary_names = ["qmlls", "qmlls6"];
 
-        if let Some(path) = worktree.which(&binary_name) {
-            self.cached_binary_path = Some(path.clone());
-            return Ok(path.clone());
+        for binary_name in binary_names.iter() {
+            if let Some(path) = worktree.which(&binary_name) {
+                self.cached_binary_path = Some(path.clone());
+                return Ok(path.clone());
+            }
         }
 
         Err(
-            "qmlls binary not found in $PATH. Make sure you have a working QT installation on your system.\nCheck the install instructions on Github for further information."
+            "qmlls binary not found in $PATH. Make sure you have a working QT installation on your system.\nCheck the install instructions on Github for further information: https://github.com/lkroll/zed-qml?tab=readme-ov-file#qmlls-lsp"
                 .to_owned(),
         )
     }
